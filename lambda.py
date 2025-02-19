f21c6301c05f554870ecbfe3e1f14469b8379cbe
btavr <brunotrodrigues98@gmail.com>
2025-02-19 17:16:52 +0000

    pop

--- lambda.py
+++ lambda.py
@@ -1,4 +1,16 @@
 my_list = [1, 2]
 
 my_list.append(3)
+print(my_list)
+
 print(my_list[0])
+
+my_list[0] = 0
+print(my_list)
+
+my_list.insert(1, 1)
+print(my_list)
+
+my_list.pop(3)
+print(my_list)
+
